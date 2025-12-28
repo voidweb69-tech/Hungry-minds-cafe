1da311f0e85bc0b0173ba8dd62771319c74f6953
voidweb69-tech <voidweb69@gmail.com>
2025-12-28 16:37:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        } else {
+          entry.target.classList.remove("active");
+        }
+      });
+    },
+    { threshold: 0.15 }
+);
+
+reveals.forEach(el => observer.observe(el));
